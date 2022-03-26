4ed429c3373e1e42c945a25ff1e1cd47efd92c39
Igor Kulgachov <duckpool@yandex.ru>
2022-03-26 18:39:45 +0300

    Initial commit

---
+++ src/main/java/com/liga/internship/bot/PrerevolutionaryTinderTgbotClientApplication.java
@@ -0,0 +1,13 @@
+package com.liga.internship.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrerevolutionaryTinderTgbotClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrerevolutionaryTinderTgbotClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liga/internship/bot/PrerevolutionaryTinderTgbotClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liga.internship.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrerevolutionaryTinderTgbotClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

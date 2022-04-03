a50819be9d4654f2e6ba2c829866b3fb8019702d
Igor Kulgachov <duckpool@yandex.ru>
2022-04-03 20:10:49 +0300

    bugfix: -ImageCreatorService update

--- src/main/java/com/liga/internship/client/service/ImageCreatorService.java
+++ src/main/java/com/liga/internship/client/service/ImageCreatorService.java
@@ -51,46 +51,38 @@ public class ImageCreatorService {
             Font headerFont = new Font("Old Standard TT", Font.BOLD, 55);
             Font descriptionFont = new Font("Old Standard TT", Font.PLAIN, 24);
             Rectangle rect = new Rectangle(image.getWidth(), image.getHeight());
-
             String headerText = formatted[0].toUpperCase(Locale.ROOT);
-            String descriptionText = formatted[1] == null ? "" : formatted[1];
-
-            AttributedString header = new AttributedString(headerText);
-            header.addAttribute(TextAttribute.FONT, headerFont);
-
-            AttributedString another = new AttributedString(descriptionText);
-            another.addAttribute(TextAttribute.FONT, descriptionFont);
-
-            AttributedCharacterIterator headerIterator = header.getIterator();
-            AttributedCharacterIterator descriptionIterator = another.getIterator();
-
-            Graphics2D g2 = (Graphics2D) image.getGraphics();
-            g2.setColor(Color.BLACK);
-
-            LineBreakMeasurer measurer1 = new LineBreakMeasurer(headerIterator, g2.getFontRenderContext());
-            LineBreakMeasurer measurer2 = new LineBreakMeasurer(descriptionIterator, g2.getFontRenderContext());
-            float textHeight = 0;
-
-            float x = 50;
-
-            List<TextLayout> lines = new ArrayList<>();
-            while (measurer1.getPosition() < headerText.length()) {
-                lines.add(measurer1.nextLayout((rect.width - x * 2)));
+            List<AttributedString> attributedStrings = new ArrayList<>();
+            for (int i = 0; i < formatted.length; i++) {
+                AttributedString attributedStringLine = new AttributedString(formatted[i]);
+                if (i == 0) {
+                    attributedStringLine.addAttribute(TextAttribute.FONT, headerFont);
+                } else {
+                    attributedStringLine.addAttribute(TextAttribute.FONT, descriptionFont);
+                }
+                attributedStrings.add(attributedStringLine);
             }
-
-            while (measurer2.getPosition() < descriptionText.length()) {
-                lines.add(measurer2.nextLayout((rect.width - x * 2)));
-            }
-
-            for (TextLayout line : lines) {
-                textHeight += line.getAscent() + line.getDescent() + line.getLeading();
-            }
-
-            float y = (rect.height - textHeight) / 2;
-
-            for (TextLayout line : lines) {
-                line.draw(g2, x, y + line.getAscent());
-                y += line.getAscent() + line.getDescent() + line.getLeading();
+            if (!attributedStrings.isEmpty()) {
+                Graphics2D g2 = (Graphics2D) image.getGraphics();
+                g2.setColor(Color.BLACK);
+                float x = 50;
+                List<TextLayout> lines = new ArrayList<>();
+                for (AttributedString attributedString : attributedStrings) {
+                    AttributedCharacterIterator iterator = attributedString.getIterator();
+                    LineBreakMeasurer measurer = new LineBreakMeasurer(iterator, g2.getFontRenderContext());
+                    while (measurer.getPosition() < headerText.length()) {
+                        lines.add(measurer.nextLayout((rect.width - x * 2)));
+                    }
+                }
+                float textHeight = 0;
+                for (TextLayout line : lines) {
+                    textHeight += line.getAscent() + line.getDescent() + line.getLeading();
+                }
+                float y = (rect.height - textHeight) / 2;
+                for (TextLayout line : lines) {
+                    line.draw(g2, x, y + line.getAscent());
+                    y += line.getAscent() + line.getDescent() + line.getLeading();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
